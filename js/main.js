de5d5f6814b05a4b4a50783650ceef633fd3792d
Xavier Rossi <xaviermagic90@gmail.com>
2022-08-23 15:47:51 +0200

    Celsius en Fahrenheit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Xavier Rossi
  * @version 0.1
- * @since   2019-08-19
+ * @since   2022-08-23
  */
 
 (function main() {
   'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+  const Celsius = Number (prompt ('Entrez un nombre :'));
 
-  console.log(a);
+  let Fahrenheit = Number ((Celsius * 9 / 5) + 32);
+  if( isNaN(Fahrenheit) || isNaN(Fahrenheit)){
+    alert('Entrez un nombre correct');
+  }else{
+    alert(`Il fait ${Fahrenheit} Fahrenheit !`);
+  }
+
 }()); // Main IIFE
+
 
